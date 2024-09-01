b2737e0e7ff8121e2e12e3fa7ce196232d89c2a2
nhathoang24 <hd3906420@gmail.com>
2024-09-01 18:42:08 +0700

    update

--- Reject serviceWorker Auto.user.js
+++ Reject serviceWorker Auto.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Reject Service Worker Auto
+// @name        Reject serviceWorker Auto
 // @namespace   rejectserviceWorkerAuto
 // @match       *://*/*
 // @run-at      document-start
@@ -7,66 +7,68 @@
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
 // @version     1.4
-// @description 10/4/2023, 5:07:06 PM
+// @description Automatically rejects service workers on matched domains
 // ==/UserScript==
 
-(() => {
-    const defaultvalue = 'none';
-    const name = 'rejectserviceWorkerAuto';
-    const prefix = "autoinject" + name;
-    const valueKey = `value${name}${document.domain}`;
-    let hostarray = JSON.parse(GM_getValue(prefix, "[]")) || [];
-    let injectedStatus = false;
+(function() {
+    const DEFAULT_VALUE = 'none';
+    const SCRIPT_NAME = 'rejectserviceWorkerAuto';
+    const PREFIX = `autoinject_${SCRIPT_NAME}`;
+    const DOMAIN_KEY = `value_${SCRIPT_NAME}_${document.domain}`;
+    const value = GM_getValue(DOMAIN_KEY, DEFAULT_VALUE);
+    let injected = false;
+    let hostArray = [];
 
     function inject() {
-        if (injectedStatus) return;
-
-        if (navigator.serviceWorker) {
-            navigator.serviceWorker.register = () => new Promise((_, rej) => rej("This method is not allowed!"));
-            window.ServiceWorkerContainer.prototype.register = () => new Promise((_, rej) => rej("This method is not allowed! 2"));
-        }
-
-        injectedStatus = true;
+        if (injected || !navigator.serviceWorker) return;
+        navigator.serviceWorker.register = () => Promise.reject("This method is not allowed!");
+        injected = true;
+        console.log('injected Reject serviceWorker Auto');
     }
 
-    function manageHost(add = true) {
-        const index = hostarray.indexOf(location.hostname);
-        if (add && index === -1) {
-            hostarray.push(location.hostname);
-        } else if (!add && index > -1) {
-            hostarray.splice(index, 1);
-        }
-        GM_setValue(prefix, JSON.stringify(hostarray));
-    }
-
-    function set() {
-        const val = parseInt(window.prompt(`Enter ${name} ${document.domain} value`, defaultvalue), 10);
-        if (!isNaN(val)) {
-            GM_setValue(valueKey, val);
+    function addHost() {
+        if (!hostArray.includes(location.hostname)) {
+            hostArray.push(location.hostname);
+            GM_setValue(PREFIX, JSON.stringify(hostArray));
+            inject();
         }
     }
 
-    function adjustValue(delta) {
-        let value = parseInt(GM_getValue(valueKey, defaultvalue), 10);
-        if (!isNaN(value)) {
-            GM_setValue(valueKey, value + delta);
+    function removeHost() {
+        const index = hostArray.indexOf(location.hostname);
+        if (index > -1) {
+            hostArray.splice(index, 1);
+            GM_setValue(PREFIX, JSON.stringify(hostArray));
         }
+    }
+
+    function setValue() {
+        const newValue = parseInt(window.prompt(`Enter ${SCRIPT_NAME} value for ${document.domain}`, DEFAULT_VALUE), 10);
+        if (!isNaN(newValue)) {
+            GM_setValue(DOMAIN_KEY, newValue);
+        }
+    }
+
+    function modifyValue(delta) {
+        const currentValue = parseInt(GM_getValue(DOMAIN_KEY, DEFAULT_VALUE), 10);
+        GM_setValue(DOMAIN_KEY, currentValue + delta);
     }
 
     try {
-        if (typeof GM_getValue(valueKey) === 'number') {
-            GM_registerMenuCommand("+", () => adjustValue(1));
-            GM_registerMenuCommand("-", () => adjustValue(-1));
+        hostArray = JSON.parse(GM_getValue(PREFIX, "[]"));
+        if (typeof value === 'number') {
+            GM_registerMenuCommand("+", () => modifyValue(1));
+            GM_registerMenuCommand("-", () => modifyValue(-1));
         }
 
-        if (hostarray.includes(location.hostname)) {
-            GM_registerMenuCommand(`Inject ${name}`, inject);
-            GM_registerMenuCommand(`Auto-Inject on ${location.hostname}`, () => manageHost(false));
+        if (hostArray.includes(location.hostname)) {
+            GM_registerMenuCommand(`Inject ${SCRIPT_NAME}`, inject);
+            GM_registerMenuCommand(`Stop Auto-Injecting on ${location.hostname}`, removeHost);
         } else {
             inject();
-            GM_registerMenuCommand(`Stop Auto-Injecting ${name}`, () => manageHost(true));
+            GM_registerMenuCommand(`Auto-Inject on ${location.hostname}`, addHost);
         }
     } catch (err) {
-        console.error(`Error adding Inject menu items: ${name}`, err);
+        console.error(`Error initializing ${SCRIPT_NAME}:`, err);
     }
 })();
